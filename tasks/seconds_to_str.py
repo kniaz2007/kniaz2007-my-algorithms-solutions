fd831119de4bd0e47b25eba185528f4c1ca6ca6d
Егор <vekaplanov@bk.ru>
2026-02-09 21:47:18 +0300

    feat: реализована функция is_prime для проверки простых чисел

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -8,8 +8,6 @@ def seconds_to_str(seconds: int) -> str:
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-
-    # Просто всегда показываем всё с нулями
     result = ""
     if days < 10: result += "0"
     result += str(days) + "d"
@@ -20,4 +18,4 @@ def seconds_to_str(seconds: int) -> str:
     if seconds < 10 and (days > 0 or hours > 0 or minutes > 0): result += "0"
     result += str(seconds) + "s"
     return result
-print(seconds_to_str(93600))
+
